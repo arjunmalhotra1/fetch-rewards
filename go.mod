7c5f7890d6d9dcb50783b6b4fd67e1e26b7dbc3d
Arjun Malhotra <am2cj@virginia.edu>
2025-04-20 23:05:47 -0700

    Add validations

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.23.5
 require (
 	github.com/go-chi/chi/v5 v5.2.1
 	github.com/go-chi/render v1.0.3
+	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/go-playground/validator/v10 v10.26.0
 )
 
@@ -18,4 +19,5 @@ require (
 	golang.org/x/net v0.34.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 )
--- main.go
+++ main.go
@@ -5,27 +5,41 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"regexp"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/render"
-	"github.com/go-playground/validator/v10"
+	"github.com/go-playground/validator"
 )
 
 type Item struct {
-	ShortDescription string `json:"shortDescription"`
-	Price            string `json:"price"`
+	ShortDescription string `json:"shortDescription" validate:"required,shortDescription"`
+	Price            string `json:"price" validate:"required,price"`
 }
 
 type Receipt struct {
-	Retailer     string `json:"retailer"`
-	PurchaseDate string `json:"purchaseDate"`
-	PurchaseTime string `json:"purchaseTime"`
-	Items        []Item `json:"items"`
-	Total        string `json:"total"`
+	Retailer     string `json:"retailer" validate:"required,retailer"`
+	PurchaseDate string `json:"purchaseDate" validate:"required,purchaseDate"`
+	PurchaseTime string `json:"purchaseTime" validate:"required,purchaseTime"`
+	Items        []Item `json:"items" validate:"required"`
+	Total        string `json:"total" validate:"required,total"`
 }
 
+// TODO: May be use contexts
+
+var validate *validator.Validate
+
 func main() {
-	validator.New()
+	validate = validator.New()
+	validate := validator.New()
+	validate.RegisterValidation("retailer", validateRetailer)
+	validate.RegisterValidation("purchaseDate", validateDateFormat)
+	validate.RegisterValidation("purchaseDate", validateTimeFormat)
+	validate.RegisterValidation("total", validateTotal)
+	validate.RegisterValidation("shortDescription", validateShortDescription)
+	validate.RegisterValidation("price", validatePrice)
+
 	router := chi.NewMux()
 
 	router.Get("/", healthz)
@@ -44,8 +58,44 @@ func processReceipts(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println(string(receiptBody))
 
+	err := validate.Struct(receipt)
+	if err != nil {
+
+	}
+
 }
 
-func validateReceipt() {
+func validateTimeFormat(fl validator.FieldLevel) bool {
+	_, err := time.Parse("15:04", fl.Field().String())
+	return err == nil
+}
 
+func validateDateFormat(fl validator.FieldLevel) bool {
+	_, err := time.Parse("2006-01-02", fl.Field().String())
+	return err == nil
+}
+
+func validateTotal(fl validator.FieldLevel) bool {
+	total := fl.Field().String()
+	matched, _ := regexp.MatchString("^\\d+\\.\\d{2}$", total)
+	return matched
+}
+
+func validateShortDescription(fl validator.FieldLevel) bool {
+	total := fl.Field().String()
+	matched, _ := regexp.MatchString("^[\\w\\s\\-]+$", total)
+	return matched
+}
+
+func validatePrice(fl validator.FieldLevel) bool {
+	total := fl.Field().String()
+	matched, _ := regexp.MatchString("^\\d+\\.\\d{2}$", total)
+	return matched
+}
+
+// Custom validation for Retailer
+func validateRetailer(fl validator.FieldLevel) bool {
+	retailer := fl.Field().String()
+	matched, _ := regexp.MatchString("^[\\w\\s\\-&]+$", retailer)
+	return matched
 }
